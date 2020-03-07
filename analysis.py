27233eabbeb4b1e4e3e5b8f08394ec9645a22a82
arch <archisha.chandel26@gmail.com>
2020-03-07 13:41:54 +0530

    adds store name and date

--- analysis.py
+++ analysis.py
@@ -1,4 +1,4 @@
-from model.extract import get_date, get_invoice_no
+from model.extract import get_date, get_invoice_no, get_store_name
 import os
 
 for fname in os.listdir('text')[:1]:
@@ -7,11 +7,15 @@ for fname in os.listdir('text')[:1]:
     folder_name = 'parse_text' + os.sep + fname
     # os.system('mkdir ' + folder_name)
     
-    # with open(folder_name+'/date.txt', 'w') as f:
-        # print(list(get_date(data)))
-        # f.writelines(list(get_date(data)))
+    with open(folder_name+'/date.txt', 'w') as f:
+        print(list(get_date(data)))
+        f.writelines(list(str(get_date(data))))
 
-    with open(folder_name+'/invoice_no.txt', 'w') as f:
-        print(list(get_invoice_no(data)))
+    with open(folder_name+'/store_name.txt', 'w') as f:
+        # print(list(get_store_name(data)))
+        f.writelines(list(str(get_store_name(data))))
+
+    # with open(folder_name+'/invoice_no.txt', 'w') as f:
+    #     print(list(get_invoice_no(data)))
         # f.writelines(list(get_date(data)))
-        # get_invoice_no(data)
+        # getdate_invoice_no(data)
--- model/extract.py
+++ model/extract.py
@@ -1,29 +1,25 @@
-
 import operator 
 import sys
 import re
 import nltk
 # from nltk.corpus import stopwords
-# from bs4 import BeautifulSoup
-# from xlrd import open_workbook
-# from xlutils.copy import copy
-import xlrd
 import os
-# import xlwt
-# from tkinter import *
-# from tkinter import ttk
 # import pandas as pd 
-# import subprocess, sys
+
 
 def get_date(items):
 	#format d/m/y & d-m-y is support
     dates=set()
+    # print(items)
     for data in items:
-        f1 = re.findall(r'\d{1,2}\/\d{1,2}\/\d{4}',data)
-        f2 = re.findall(r'\d{4}\/\d{1,2}\/\d{1,2}',data)
-        f3 = re.findall(r'\d{2}\/\d{1,2}\/\d{1,2}',data)
-        f4 = re.findall(r'\d{1,2}\/\d{1,2}\/\d{2}',data)
-        
+        f1 = re.findall(r'\d{1,2}\d[- /.]\d{1,2}\d[- /.]\d{4}',data)
+        f2 = re.findall(r'\d{4}\d[- /.]\d{1,2}\d[- /.]\d{1,2}',data)
+        f3 = re.findall(r'\d{2}\d[- /.]\d{1,2}\d[- /.]\d{1,2}',data)
+        f4 = re.findall(r'\d{1,2}\d[- /.]\d{1,2}\d[- /.]\d{2}',data)
+        f5 = re.findall(r'(Jan(uary)?|Feb(ruary)?|Mar(ch)?|Apr(il)?|May|Jun(e)?|Jul(y)?|Aug(ust)?|Sep(tember)?|Oct(ober)?|Nov(ember)?|Dec(ember)?)\s+\d{1,2},\s+\d{4}',data)
+        f6 = re.findall(r'\d{1,2}[,/-](Jan(uary)?|Feb(ruary)?|Mar(ch)?|Apr(il)?|May|Jun(e)?|Jul(y)?|Aug(ust)?|Sep(tember)?|Oct(ober)?|Nov(ember)?|Dec(ember)?)[,/-]\d{4}',data)
+        f7 = re.findall(r'\d{1,2}[,/-](Jan(uary)?|Feb(ruary)?|Mar(ch)?|Apr(il)?|May|Jun(e)?|Jul(y)?|Aug(ust)?|Sep(tember)?|Oct(ober)?|Nov(ember)?|Dec(ember)?)[,/-]\d{2}',data)
+         
         for i in f1:
             dates.add(i)
         for i in f2:
@@ -32,4 +28,36 @@ def get_date(items):
             dates.add(i)
         for i in f4:
             dates.add(i)
+        for i in f5:
+            dates.add(i)
+        for i in f6:
+            dates.add(i)
+        for i in f7:
+            dates.add(i)
+        
     return dates
+
+def get_store_name(data):
+    store_name = set()
+    existing_stores = os.listdir(r'Training Data Set')
+    print(data)
+    print(existing_stores)
+    print([x for x in data if re.search('PHOENIX MALL', x)])
+    # sequence = 'PHOENIX MALL'
+    # rgx = re.compile('.*'.join(re.escape(x) for x in sequence))
+
+def get_invoice_no(data):
+    print(data)
+    invoice_no = set()
+    for i in data:
+        if 'invoice' in i.lower().strip().split():
+            print(i)
+        if 'bill' in i.lower().strip().split():
+            print(i)
+            for words in i.lower().strip().split():
+                try:
+                    x = int(words) + 0
+                    invoice_no.add(x)
+                except:
+                    continue
+    return invoice_no
